96c71389c4b322df9d5d005187168595818e3228
Alisher <88431330+mightcoding@users.noreply.github.com>
2024-05-01 20:33:14 +0500

    Update test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,10 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_length():
+    password = generate_password(15)
+    assert len(password) == 15
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
